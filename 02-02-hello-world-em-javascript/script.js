5236ef52c3d4ca19e4ffd730ce936920251ae520
Warlley Lopes <warlleylopes123@gmail.com>
2023-02-24 13:32:37 -0300

    revisando o curso aula 01-02

--- 02-02-hello-world-em-javascript/script.js
+++ 02-02-hello-world-em-javascript/script.js
@@ -1,2 +1,18 @@
-let nome = "World";
-console.log(`Hello, ${nome}!`);
+let saudacao = "World";
+console.log(`Hello, ${saudacao}!`);
+
+let world = "Mundo";
+console.log(`Olá, ${world}!`);
+
+let eu = "Warlley";
+console.log(`Olá Sr.${eu}!`);
+
+let nome = "Warlley";
+let sobrenome = "Lopes";
+
+console.log(`Olá Sr.${nome + " " + sobrenome}`);
+
+let name = "Warlley";
+let lastName = "Lopes";
+
+console.log(`Hello, Mr.${name + " " + lastName}`);
